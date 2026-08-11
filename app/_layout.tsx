29264e97893d1d85f35f63ddae5b8912b6d3625f
safesteps <kmwstudy88@gmail.com>
2026-08-11 20:43:46 +1000

    Keep canonical intake folder route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -84,7 +84,6 @@ export default function RootLayout() {
                         <Stack.Screen name="carer/index" />
                         <Stack.Screen name="advocate/index" />
                         <Stack.Screen name="assessments/index" />
-                        <Stack.Screen name="intake" />
                         <Stack.Screen name="intake-progress" />
                         <Stack.Screen name="intake-review" />
                         <Stack.Screen name="assessment-system/index" />
